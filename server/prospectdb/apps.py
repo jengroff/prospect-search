f0910f8add0bc6d5c860412761dd19c9e2960c31
Josh Engroff <jengroff@alumni.princeton.edu>
2021-10-02 20:20:10 -0400

    first commit; server setup and structure

---
+++ server/prospectdb/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ProspectdbConfig(AppConfig):
+    name = 'prospectdb'
---
+++ server/start.sh
@@ -0,0 +1,6 @@
+# server/start.sh
+
+#!/bin/bash
+
+python ./manage.py migrate
+python ./manage.py runserver 0.0.0.0:8000
