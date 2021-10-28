f97e908b376620696c2cb0c178d5249bdc2def8f
Josh Engroff <jengroff@alumni.princeton.edu>
2021-10-28 08:05:59 -0400

    added production env settings

--- server/prospectdb/urls.py
+++ server/prospectdb/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 
-from .views import ProspectView, TeamView, StudyView, ConversationMessageView
+from .views import ProspectView, TeamView, StudyView, ConversationMessageView, ESMessagesView
 
 
 urlpatterns = [
     path('prospects/', ProspectView.as_view()),
     path('teams/', TeamView.as_view()),
     path('studies/', StudyView.as_view()),
-    path('messages/', ConversationMessageView.as_view()),
+    path('messages/', ESMessagesView.as_view()),
+    path('es-messages', ESMessagesView.as_view()),
+    path('pg-messages/', ConversationMessageView.as_view())
 ]
